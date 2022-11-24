e84a2299fdb07471fdf28c07b4ee5d60668d77c4
UihwanLee <luh5063014@naver.com>
2022-11-24 19:19:51 +0900

    Upload problem

--- 단계/12단계 브루트 포스/05_영화감독 숌.cpp
+++ 단계/12단계 브루트 포스/05_영화감독 숌.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
-#include <vector> 
 
 using namespace std;
 
-int main(){
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+    int N;
+    cin >> N;
     
+    int count=0;
+    int i = 666;
+    
+    // 666 1666 .. 5666 6661 6662 .. 7666 8666 .. 10666 15666 .. 16661
+    while(true)
+    {
+    	int tmp = i;
+    	while(tmp != 0)
+    	{
+    		if(tmp%1000 == 666)
+    		{
+    			count++;
+    			break;
+			}
+			tmp /= 10;
+		}
+		if(N==count) break;
+    	
+    	i++;
+	}
+	
+	cout << i << '\n';
     
     return 0;
 }
