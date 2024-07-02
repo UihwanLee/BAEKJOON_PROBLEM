80247098ae5194223f0ea6e0e0da0269aacc326f
Lee Ui Hwan <luh5063014@naver.com>
2024-07-02 20:11:23 +0900

    Upload problem

---
+++ 단계/1단계 입출력과 사칙연산/12_꼬마정민.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+int main(void)
+{
+	long long A, B, C;
+	
+	cin >> A >> B >> C;
+	
+	cout << A + B + C;
+	
+	return 0;	
+} 	
+
