ca8d026e1ea09ba5ec2a25a9dd383ee0859d1892
Lee Ui Hwan <luh5063014@naver.com>
2024-07-07 23:46:42 +0900

    Upload problem

---
+++ 단계/6단계 문자열/10_문자와 문자열.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+int main(void)
+{
+	char S[1000];
+	int i;
+	
+	cin >> S >> i;
+	
+	cout << S[i-1];
+	
+	return 0;	
+} 	
