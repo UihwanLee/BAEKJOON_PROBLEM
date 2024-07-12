96d1ee3ac350569a6ddd299580adea15a1e834cb
Lee Ui Hwan <luh5063014@naver.com>
2024-07-13 00:01:27 +0900

    Update 14_별 찍기 - 7.cpp

--- 단계/6단계 문자열/14_별 찍기 - 7.cpp
+++ 단계/6단계 문자열/14_별 찍기 - 7.cpp
@@ -11,14 +11,22 @@ int main() {
     int N;
     
     cin >> N;
-
-    for (int i = 0; i < N; i++){
-        cout << string(N-(i+1),' ') << string(2*i+1,'*') << string(N-(i+1),' ') << endl;
-    }
-
-    for (int i = N-1; i >= 1; i--){   
-        cout << string((N-i),' ') << string(2*i-1,'*') << string((N-i),' ') << endl;
-    }
+    
+    for(int i=1; i<=N; i++) {
+    	for(int j=0; j<i; j++) {
+    		cout << '*';
+		}
+		
+		cout << endl;
+	}
+	
+	for(int i=N-1; i>0; i--) {
+    	for(int j=0; j<; j++) {
+    		cout << '*';
+		}
+		
+		cout << endl;
+	}
     
 	return 0; 
 }
